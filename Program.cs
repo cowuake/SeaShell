259769e65ba45045620c0710cfb5c83ba0f41f0c
r-mura <riccardo_mura@yahoo.com>
2022-02-21 10:05:21 +0100

    A bit of restructuring

--- Program.cs
+++ Program.cs
@@ -1,93 +1,95 @@
 ﻿using System.Diagnostics;
 
-namespace SeaShell {
-    class Program
+class Program
+{
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            var shell = new SeaShell();
+        var shell = new SeaShell();
+        try { shell.Run(); } catch {}
+    }
+}
 
-            try
+public class SeaShell
+{
+    private static void cow()
+    { Console.WriteLine("🐮 There is no cow level 🐮"); }
+
+    private Dictionary<string, Action> Builtin = new Dictionary<string, Action>()
+    {
+        { "beep", () => Console.Beep() },
+        { "cow", () => cow() },
+        { "exit", () => Environment.Exit(0) },
+        { "quit", () => Environment.Exit(0) },
+    };
+
+    private void WriteColored(string text, ConsoleColor color, bool newLine = false)
+    {
+        Console.ForegroundColor = color;
+        if (newLine) { Console.WriteLine(text); } else { Console.Write(text); }
+        Console.ResetColor();
+    }
+
+    private (string, string) Prompt()
+    {
+    //Console.WriteLine("[{0}@{1}]", Environment.UserName, Environment.MachineName);
+        Console.Write(">> ");
+        var input = Console.ReadLine().Trim().Split(' ');
+        var command = input[0];
+
+        for (int i = 1; i < input.Length-1; i++)
+        {
+            // Clean command and arguments
+            input[i].Trim();
+        }
+        var args = string.Join(" ", input[1..]);
+
+        return (command, args);
+    }
+
+    public void Run()
+    {
+        string command = "", args = "";
+
+        while (true)
+        {
+            (command, args) = Prompt();
+
+            if (Builtin.ContainsKey(command))
             {
-                shell.Run();
+                Builtin[command]();
+            } else {
+                try
+                {
+                    var exitStatus = Exec(command, args);
+                } catch {
+                    string message = "Command not found: ";
+                    string filler = "";
+
+                    if ((message+command+" "+args).Length > Console.BufferWidth)
+                    {
+                        filler = "\n";
+                    } else {
+                        filler = " ";
+                    }
+
+                    WriteColored(message, ConsoleColor.Red);
+                    Console.WriteLine("{0}{1} {2}", filler, command, args);
+                }
             }
-            catch {}
         }
     }
 
-    public class SeaShell
+    private int Exec(string command, string args)
     {
-        private static void cow()
-        { Console.WriteLine("🐮 There is no cow level 🐮"); }
+        var proc = new Process();
 
-        private Dictionary<string, Action> Builtin = new Dictionary<string, Action>()
-        {
-            { "cow", () => cow() },
-        };
+        proc.StartInfo.FileName = command;
+        proc.StartInfo.Arguments = args;
 
-        private (string, string) Prompt()
-        {
-        //Console.WriteLine("[{0}@{1}]", Environment.UserName, Environment.MachineName);
-            Console.Write(">> ");
-            var input = Console.ReadLine().Trim().Split(' ');
-            var command = input[0];
+        // Spawn the process
+        proc.Start();
+        proc.WaitForExit();
 
-            for (int i = 1; i < input.Length-1; i++)
-            {
-                // Clean command and arguments
-                input[i].Trim();
-            }
-            var args = string.Join(" ", input[1..]);
-
-            return (command, args);
-        }
-        public void Run()
-        {
-            string command = "", args = "";
-
-            do
-            {
-                (command, args) = Prompt();
-
-                if (Builtin.ContainsKey(command))
-		{
-                    Builtin[command]();
-                } else {
-                    try
-                    {
-                        var exitStatus = Exec(command, args);
-                    } catch {
-                        string message = "Command not found: ";
-                        string filler = "";
-
-                        if ((message+command+" "+args).Length > Console.BufferWidth )
-                        {
-                            filler = "\n";
-                        } else {
-                            filler = " ";
-                        }
-
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        Console.Write(message);
-                        Console.ResetColor();
-                        Console.WriteLine("{0}{1} {2}", filler, command, args);
-                    }
-                }
-
-            } while (command != "exit" && command != "quit");
-        }
-
-        public int Exec(string command, string args)
-        {
-            var proc = new Process();
-
-            proc.StartInfo.FileName = command;
-            proc.StartInfo.Arguments = args;
-
-            // Spawn the process
-            proc.Start();
-            proc.WaitForExit();
-            return 0;
-        }
+        return 0;
     }
 }
